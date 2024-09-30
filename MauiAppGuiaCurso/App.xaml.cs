48ab0d361362d627703e03805ba505392a79719b
Usuario <Usuario@CPD03-M14>
2024-09-30 14:18:00 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppGuiaCurso/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppGuiaCurso
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppGuiaCurso/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppGuiaCurso
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
